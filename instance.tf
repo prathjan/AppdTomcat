bc50d2ce231fa6f8d3ef653acf9ef47b83c2c1a7
prathjan <prathjan@cisco.com>
2021-08-12 08:22:04 -0700

    appwars

--- instance.tf
+++ instance.tf
@@ -296,7 +296,7 @@ resource "null_resource" "vm_node_init" {
 
   provisioner "file" {
     source = "scripts/context.xml"
-    destination = "/tmp/conbtext.xnml"
+    destination = "/tmp/context.xml"
     connection {
       type = "ssh"
       host = "${vsphere_virtual_machine.vm_deploy[count.index].default_ip_address}"
@@ -307,6 +307,18 @@ resource "null_resource" "vm_node_init" {
     }
   }
 
+  provisioner "file" {
+    source = "appwars/*"
+    destination = "/tmp/*"
+    connection {
+      type = "ssh"
+      host = "${vsphere_virtual_machine.vm_deploy[count.index].default_ip_address}"
+      user = "root"
+      password = "${var.root_password}"
+      port = "22"
+      agent = false
+    }
+  }
 
   provisioner "remote-exec" {
     inline = [
