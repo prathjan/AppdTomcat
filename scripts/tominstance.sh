fc574fc58e97f075cf22d8e3e9752fc2265c4623
prathjan <prathjan@cisco.com>
2021-08-15 07:56:09 -0700

    added service file

--- scripts/tominstance.sh
+++ scripts/tominstance.sh
@@ -22,6 +22,10 @@ sed 's/svcname/'$1'/g' /tmp/startup.sh > /tmp/output2.file
 cp /tmp/output2.file /tmp/startup.sh
 sed 's/svcname/'$1'/g' /tmp/shutdown.sh > /tmp/output3.file
 cp /tmp/output3.file /tmp/shutdown.sh
+sed 's/svcname/'$1'/g' /tmp/service > /tmp/output5.file
+cp /tmp/output5.file '/tmp/'$1'.service'
+
+
 cp /tmp/shutdown.sh '/usr/local/apache/'$1'/bin'
 cp /tmp/startup.sh '/usr/local/apache/'$1'/bin'
 cp /tmp/server.xml '/usr/local/apache/'$1'/conf'
@@ -30,5 +34,6 @@ cp '/tmp/'$4'' '/usr/local/apache/'$1'/webapps'
 touch '/usr/local/apache/'$1'/logs/catalina.out'
 chmod +x '/usr/local/apache/'$1'/bin/startup.sh'
 chmod +x '/usr/local/apache/'$1'/bin/shutdown.sh'
-sh '/usr/local/apache/'$1'/bin/startup.sh'
+cp '/tmp/'$1'.service' '/etc/systemd/system/'$1''
+sudo systemctl start '/etc/systemd/system/'$1''
 
