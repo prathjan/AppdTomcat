2d5c04e0e6791b9b913db3b8869f5c834d8ceaf1
prathjan <prathjan@cisco.com>
2021-08-15 09:34:31 -0700

    added service file

--- instance.tf
+++ instance.tf
@@ -293,6 +293,19 @@ resource "null_resource" "vm_node_init" {
     }
   }
 
+  provisioner "file" {
+    source = "scripts/service"
+    destination = "/tmp"
+    connection {
+      type = "ssh"
+      host = "${vsphere_virtual_machine.vm_deploy[count.index].default_ip_address}"
+      user = "root"
+      password = "${var.root_password}"
+      port = "22"
+      agent = false
+    }
+  }
+
   provisioner "remote-exec" {
     inline = [
         "chmod +x /tmp/tominstance.sh",
