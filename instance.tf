7393b57044abae9022835ce1fbee3ffb1b9f2359
prathjan <prathjan@cisco.com>
2021-08-28 14:55:45 -0700

    fix to end chars

--- instance.tf
+++ instance.tf
@@ -242,6 +242,7 @@ resource "null_resource" "vm_node_init" {
         "chmod +x /tmp/rbac.sh",
         "${local.download}",
 	"/tmp/rbac.sh",
+	"dos2unix /home/ec2-user/environment/workshop/application.env",
 	". /home/ec2-user/environment/workshop/application.env",
 	"echo echoing install",
 	"echo ${local.install}",
